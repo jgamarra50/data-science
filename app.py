5136744d453965617959a96ef51905dc3f5b9588
Alfredo Antonio Diaz Claro <alfredoa.diazc@gmail.com>
2025-10-02 08:39:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import os
 import pandas as pd
 
 # Mostrar logo
-st.image("logo.gif", use_container_width=True)
+st.image("logo.gif", width='stretch')
 
 # Título
 st.title("🧮 Reconocimiento de números escritos a mano - Alfredo Díaz Claro 2025")
@@ -47,7 +47,7 @@ if not all([modelo_d1, modelo_c2, modelo_c3]):
 st.subheader("🖌️ Dibuja un número")
 canvas_result = st_canvas(
     fill_color="white",
-    stroke_width=10,
+    stroke_width=12,
     stroke_color="black",
     background_color="white",
     height=280,
@@ -125,5 +125,6 @@ if st.button("Predecir"):
 
     else:
         st.warning("⚠️ Por favor, dibuja un número antes de predecir.")
+
 
 
